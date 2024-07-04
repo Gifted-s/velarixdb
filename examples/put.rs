2168b244cfbedffffc3aa761a8e46259c7c846c6
Adewumi Sunkanmi <sunkanmiadewumi1@gmail.com>
2024-07-04 23:40:51 +0100

    [Documentation] Introduced rust docs

---
+++ examples/put.rs
@@ -0,0 +1,15 @@
+// use std::path::PathBuf;
+
+// use tempfile::tempdir;
+// use vikingsdb::db::DataStore;
+
+fn main() {
+  
+}
+
+// async fn put() {
+//     let root = tempdir().unwrap();
+//     let path = PathBuf::from(root.path().join("db"));
+//     let store = DataStore::new("put_db", path.clone()).await.unwrap();
+//     store.put()
+// }
